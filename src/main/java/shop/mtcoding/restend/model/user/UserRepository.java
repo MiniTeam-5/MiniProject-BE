75a7d571b69cfd4b88feead82c5679b33fe01284
CHITSOO <jisu3148496@naver.com>
2023-05-04 23:34:00 +0900

    잠시

--- src/main/java/shop/mtcoding/restend/model/user/UserRepository.java
+++ src/main/java/shop/mtcoding/restend/model/user/UserRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.security.core.parameters.P;
+import shop.mtcoding.restend.model.leave.enums.LeaveStatus;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -14,4 +17,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query("select u from User u where u.email = :email")
     Optional<User> findByEmail(@Param("email") String email);
+
+    @Query("select u from User u inner join Leave l on u.id = l.user.id where where l.startDate = :today and l.status = :waiting")
+    List<User> findByStartDateAndStatus(LocalDate today, LeaveStatus waiting);
+
+
 }
