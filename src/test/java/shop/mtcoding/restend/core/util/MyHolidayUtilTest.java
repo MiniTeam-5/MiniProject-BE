1acfeedd511817a0ef52f4ce7742d2c4e232ed21
CHITSOO <jisu3148496@naver.com>
2023-05-05 23:22:03 +0900

    Feat: MyHolidayUtilTest 완성

--- src/test/java/shop/mtcoding/restend/core/util/MyHolidayUtilTest.java
+++ src/test/java/shop/mtcoding/restend/core/util/MyHolidayUtilTest.java
@@ -1,11 +1,14 @@
 package shop.mtcoding.restend.core.util;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import java.net.URISyntaxException;
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 
 
@@ -16,12 +19,16 @@ class MyHolidayUtilTest {
 
     @Test
     void getHolidays_test() throws URISyntaxException {
+        // given
+        String year = "2023";
+        String month = "05";
+        Set<LocalDate> expected = new HashSet<>(Arrays.asList(LocalDate.parse("2023-05-29"),
+                LocalDate.parse("2023-05-27"), LocalDate.parse("2023-05-05")));
 
-        Set<LocalDate> actualResponse = myHolidayUtil.getHolidays("2023", "05");
-        System.out.println("테스트 : ");
-        for (LocalDate item : actualResponse) {
-            System.out.println(item);
-        }
-        //Set{2023-05-29, 2023-05-27, 2023-05-05}
+        // when
+        Set<LocalDate> actual = myHolidayUtil.getHolidays("2023", "05");
+
+        // then
+        Assertions.assertThat(expected).isEqualTo(expected);
     }
 }
