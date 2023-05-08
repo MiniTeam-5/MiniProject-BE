24d8c45c19b40d7c129b6f42290c6b448deaddc3
zin <zidanemook@naver.com>
2023-05-08 17:23:18 +0900

    Feat: 테스트 코드작업

---
+++ src/test/java/shop/mtcoding/restend/service/RefreshServiceTest.java
@@ -0,0 +1,23 @@
+package shop.mtcoding.restend.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
+class RefreshServiceTest {
+
+    @Test
+    void 액세스재발급() {
+
+
+    }
+
+    @Test
+    void 리프레시토큰회수() {
+    }
+}
